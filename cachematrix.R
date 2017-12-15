5a7be006ff52d46f469d4f8f22d02b81776e716a
scanthony <fandideyang@hotmail.com>
2017-12-15 13:26:26 +0800

    Submission Version 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The first function creates a special object, which contains a matrix 
+## and stores a cache of the inversed matrix as well.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inversed <- NULL
+	set <- function(mat) {
+		x <<- mat
+		inversed <<- NULL
+	}
+	get <- function() x
+	setinversed <- function(inverse) inversed <<- inverse
+	getinversed <- function() inversed
+	list(set = set, get = get, setinversed = setinversed, getinversed = getinversed)
 }
 
 
-## Write a short comment describing this function
+## The second function first checked if there is a cached result.
+## If there is one, then return it. 
+## If there is none, caculate it first, store it and then return it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inversed <- x$getinversed()
+		if (!is.null(inversed)) {
+			print("Cached result queried!")
+			return(inversed)
+		}
+		mat <- x$get()
+		inversed <- solve(mat, ...)
+		x$setinversed(inversed)
+		return(inversed)
 }
+
+## The following scripts checks if the the code above works.
+mat <- matrix(rnorm(16),4,4)
+mat
+mat_with_cache <- makeCacheMatrix(mat)
+cacheSolve(mat_with_cache)
